c4ef450e31f61df4fedb2bb271b7a2397a380218
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-23 10:40:19 +0530

    made board state

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Square } from './Square';
 
 export const Board = () => {
+  const [board, setBoard] = useState(Array(9).fill(null));
+
+  const handleClick = position => {
+    setBoard(prev => {
+      return prev.map((square, pos) => {
+        if (pos === position) {
+          return 'X';
+        }
+        return square;
+      });
+    });
+  };
+
+  const renderSquare = position => {
+    return (
+      <Square value={board[position]} onClick={() => handleClick(position)} />
+    );
+  };
   return (
     <div className="board">
       <div className="board-row">
-        <Square value={1} />
-        <Square value={2} />
-        <Square value={3} />
+        {renderSquare(0)}
+        {renderSquare(1)}
+        {renderSquare(2)}
       </div>
       <div className="board-row">
-        <Square value={4} />
-        <Square value={5} />
-        <Square value={6} />
+        {renderSquare(3)}
+        {renderSquare(4)}
+        {renderSquare(5)}
       </div>
       <div className="board-row">
-        <Square value={7} />
-        <Square value={8} />
-        <Square value={9} />
+        {renderSquare(6)}
+        {renderSquare(7)}
+        {renderSquare(8)}
       </div>
     </div>
   );
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-export const Square = ({ value }) => {
+export const Square = ({ value, onClick }) => {
   return (
-    <button type="button" className="square">
+    <button type="button" className="square" onClick={onClick}>
       {value}
     </button>
   );
