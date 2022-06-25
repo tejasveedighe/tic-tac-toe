32c207f7b8c6a9de5bf0c5a968dcf4c058d5b434
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-06-25 09:13:07 +0530

    added styles and changed the reset button on tie"

--- src/App.jsx
+++ src/App.jsx
@@ -44,20 +44,30 @@ export default function App() {
     setCurrentMove(0);
   };
 
+  const noMovesLeft = current.board.every(el => el !== null);
+
   return (
     <div className="app">
-      <h1>TIC TAC TOE</h1>
-      <Message winner={winner} current={current} />
+      <h1>
+        TIC <span className="text-green">TAC</span> TOE
+      </h1>
+      <Message winner={winner} current={current} noMovesLeft={noMovesLeft} />
       <Board
         board={current.board}
         handleClick={handleClick}
         winningSquares={winningSquares}
       />
-      <button type="button" onClick={() => setNewGame()}>
+      <button
+        className={`btn-reset ${winner || noMovesLeft ? 'active' : ''}`}
+        type="button"
+        onClick={() => setNewGame()}
+      >
         {' '}
         Start New Game
       </button>
+      <h2 style={{ fontWeight: 'normal' }}>Current Game History</h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls" />
     </div>
   );
 }
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -4,9 +4,10 @@ export const Square = ({ value, onClick, isWinningSquare }) => {
   return (
     <button
       type="button"
-      className="square"
+      className={`square ${isWinningSquare ? 'winning' : ''} ${
+        value === 'X' ? 'text-green' : 'text-orange'
+      }`}
       onClick={onClick}
-      style={{ fontWeight: isWinningSquare ? 'bold' : 'normal' }}
     >
       {value}
     </button>
